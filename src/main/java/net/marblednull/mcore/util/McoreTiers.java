d7d02ee0d30e5a9de50e0ea3856cbff419178665
MarbledNull <cyandoezstuff@gmail.com>
2024-10-05 14:11:09 -0500

    forgot

--- src/main/java/net/marblednull/mcore/util/McoreTiers.java
+++ src/main/java/net/marblednull/mcore/util/McoreTiers.java
@@ -15,11 +15,11 @@ public class McoreTiers {
 
     public static final Tier STEEL = TierSortingRegistry.registerTier(
             new ForgeTier(3, 1561, 9f, 4f, 14,
-                    ModTags.Blocks.NEEDS_STEEL_TOOL, () -> Ingredient.of(ModItems.STEEL_INGOT.get())),
+                    McoreTags.Blocks.NEEDS_STEEL_TOOL, () -> Ingredient.of(ModItems.STEEL_INGOT.get())),
             new ResourceLocation(MarbledsCore.MODID, "steel"), List.of(Tiers.IRON), List.of());
 
     public static final Tier TITANIUM = TierSortingRegistry.registerTier(
             new ForgeTier(3, 250, 6f, 2f, 14,
-                    ModTags.Blocks.NEEDS_TITANIUM_TOOL, () -> Ingredient.of(ModItems.TITANIUM_INGOT.get())),
+                    McoreTags.Blocks.NEEDS_TITANIUM_TOOL, () -> Ingredient.of(ModItems.TITANIUM_INGOT.get())),
             new ResourceLocation(MarbledsCore.MODID, "titanium"), List.of(Tiers.IRON), List.of());
 }
