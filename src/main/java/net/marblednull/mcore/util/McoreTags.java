66e04170f8692c657eef220ae1c2d2f0523612e3
MarbledNull <cyandoezstuff@gmail.com>
2024-10-05 14:10:58 -0500

    renamed ModTags to McoreTags

--- src/main/java/net/marblednull/mcore/util/McoreTags.java
+++ src/main/java/net/marblednull/mcore/util/McoreTags.java
@@ -8,7 +8,7 @@ import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 
-public class ModTags {
+public class McoreTags {
     public static class Blocks {
         public static final TagKey<Block> NEEDS_TITANIUM_TOOL = tag("needs_titanium_tool");
         public static final TagKey<Block> NEEDS_STEEL_TOOL = tag("needs_steel_tool");
