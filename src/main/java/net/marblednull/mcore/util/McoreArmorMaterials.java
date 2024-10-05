b330d5dfe4a2a413e0779a8fb6f085b93f0e0dbc
MarbledNull <cyandoezstuff@gmail.com>
2024-10-05 11:30:07 -0500

    armor materials

---
+++ src/main/java/net/marblednull/mcore/util/McoreArmorMaterials.java
@@ -0,0 +1,74 @@
+package net.marblednull.mcore.util;
+
+import net.marblednull.mcore.MarbledsCore;
+import net.marblednull.mcore.init.ModItems;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.util.LazyLoadedValue;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.crafting.Ingredient;
+
+import java.util.function.Supplier;
+
+public enum McoreArmorMaterials implements ArmorMaterial {
+    STEEL("steel", 33, new int[]{2, 6, 5, 2}, 15, SoundEvents.ARMOR_EQUIP_NETHERITE,
+            0.0F, 0.0F, () -> Ingredient.of(ModItems.STEEL_INGOT.get())),
+
+    TITANIUM("titanium", 15, new int[]{3, 6, 8, 3}, 10, SoundEvents.ARMOR_EQUIP_NETHERITE,
+            3.0F, 0.1F, () -> Ingredient.of(ModItems.TITANIUM_INGOT.get()));
+
+    private static final int[] BASE_DURABILITY = {13, 15, 16, 11};
+    private final String name;
+    private final int durabilityMultiplier;
+    private final int[] slotProtections;
+    private final int enchantmentValue;
+    private final SoundEvent sound;
+    private final float toughness;
+    private final float knockbackResistance;
+    private final LazyLoadedValue<Ingredient> repairIngredient;
+
+    McoreArmorMaterials(String p_40474_, int p_40475_, int[] p_40476_, int p_40477_,
+                      SoundEvent p_40478_, float p_40479_, float p_40480_, Supplier<Ingredient> p_40481_) {
+        this.name = p_40474_;
+        this.durabilityMultiplier = p_40475_;
+        this.slotProtections = p_40476_;
+        this.enchantmentValue = p_40477_;
+        this.sound = p_40478_;
+        this.toughness = p_40479_;
+        this.knockbackResistance = p_40480_;
+        this.repairIngredient = new LazyLoadedValue<>(p_40481_);
+    }
+
+    public int getDurabilityForSlot(EquipmentSlot pSlot) {
+        return BASE_DURABILITY[pSlot.getIndex()] * this.durabilityMultiplier;
+    }
+
+    public int getDefenseForSlot(EquipmentSlot pSlot) {
+        return this.slotProtections[pSlot.getIndex()];
+    }
+
+    public int getEnchantmentValue() {
+        return this.enchantmentValue;
+    }
+
+    public SoundEvent getEquipSound() {
+        return this.sound;
+    }
+
+    public Ingredient getRepairIngredient() {
+        return this.repairIngredient.get();
+    }
+
+    public String getName() {
+        return MarbledsCore.MODID + ":" + this.name;
+    }
+
+    public float getToughness() {
+        return this.toughness;
+    }
+
+    public float getKnockbackResistance() {
+        return this.knockbackResistance;
+    }
+}
