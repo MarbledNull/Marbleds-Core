ef376f83c656778789a5e6bbf676eedd2d0dee18
MarbledNull <cyandoezstuff@gmail.com>
2024-10-06 09:35:45 -0500

    creative tab

--- src/main/java/net/marblednull/mcore/init/ModTabs.java
+++ src/main/java/net/marblednull/mcore/init/ModTabs.java
@@ -4,7 +4,7 @@ import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 
 public class ModTabs {
-    public static final CreativeModeTab TABS     = new CreativeModeTab("mcore") {
+    public static final CreativeModeTab TABS = new CreativeModeTab("mcore") {
         @Override
         public ItemStack makeIcon() {
             return new ItemStack(ModItems.STEEL_BLOCK.get());
