c5c361beeed0413406c24d08a58ff82848302d55
MarbledNull <cyandoezstuff@gmail.com>
2024-10-17 22:02:57 -0500

    tiers

--- src/main/java/net/marblednull/mcore/util/McoreTiers.java
+++ src/main/java/net/marblednull/mcore/util/McoreTiers.java
@@ -14,23 +14,10 @@ import java.util.function.Supplier;
 
 public enum McoreTiers implements IItemTier {
 
-    /*public static final Tier;STEEL = TierSortingRegistry.registerTier(
-            new ForgeTier(3, 1561, 9f, 4f, 14,
-                    McoreTags.Blocks.NEEDS_STEEL_TOOL, () -> Ingredient.of(ModItems.STEEL_INGOT.get())),
-            new ResourceLocation(MarbledsCore.MODID, "steel"), List.of(Tier.IRON), List.of());
-
-    public static final Tier TITANIUM = TierSortingRegistry.registerTier(
-            new ForgeTier(3, 250, 6f, 2f, 14,
-                    McoreTags.Blocks.NEEDS_TITANIUM_TOOL, () -> Ingredient.of(ModItems.TITANIUM_INGOT.get())),
-            new ResourceLocation(MarbledsCore.MODID, "titanium"), List.of(Tiers.IRON), List.of());
-*/
-
     STEEL(3, 1561, 9f, 4f, 14,
             () -> Ingredient.of(ModItems.STEEL_INGOT.get())),
     TITANIUM(3, 250, 6f, 2f, 14,
                   () -> Ingredient.of(ModItems.TITANIUM_INGOT.get()));
-
-    ;
 
     private final int level;
     private final int uses;
